081e45387c9b9d5ec2993447d23fac5baf77087c
saa27 <saakshi27050@gmail.com>
2020-10-29 19:33:46 +0530

    added tree question

---
+++ TREE_symmetricTree.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSame(TreeNode *p, TreeNode *q){
+        if(p == NULL && q == NULL)
+            return true;
+        else if(p == NULL || q == NULL)
+            return false;
+        return(p-> val == q->val && isSame(p->left, q->right) && isSame(p->right, q->left));
+    }
+    
+    bool isSymmetric(TreeNode* root) {
+        if(root == NULL)
+            return true;
+        return isSame(root->left, root->right);
+    }
+};
