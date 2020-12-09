8968f7e9b7c0408848c759cd706f7c50290cc109
saa27 <saakshi27050@gmail.com>
2020-12-09 22:47:23 +0530

    :white_check_mark: Recursion ques
    2

---
+++ Recursion_generateBalancedParantheses.cpp
@@ -0,0 +1,49 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+    void solve(int open,int close,string op,vector<string> &v){
+        if(open == 0 && close == 0){
+            v.push_back(op);
+            return;
+        }
+        if(open !=0){
+            string op1 = op;
+            op1.push_back('(');
+            solve(open-1,close,op1,v);
+        }
+        if(close>open){
+            string op2 = op;
+            op2.push_back(')');
+            solve(open,close-1,op2,v);
+        }
+        return;
+    }
+    
+    vector<string> generateParenthesis(int n) {
+        vector<string> v;
+        int open=n;
+        int close=n;
+        string op="";
+        solve(open,close,op,v);
+        return v;
+    }
+    
+    
+
+    int main(){
+        int n;
+        int i=0;
+        cin>>n;
+        vector<string> v;
+        v= generateParenthesis(n);
+        while(v.size()){
+            cout<<v[i]<<"\n";
+            i++;
+        }
+    }
---
+++ Recursion_letterCasePermutation.cpp
@@ -0,0 +1,42 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+void permutationCase(string ip,string op){
+    if(ip.length() == 0){//when the input is "", the op will be the answer
+        cout<<op<<" ";
+        return;
+    }
+    
+    if(isalpha(ip[0])){
+        string op1 = op;
+        string op2 = op;
+        op1.push_back(tolower(ip[0]));
+        op2.push_back(toupper(ip[0]));
+        ip.erase(ip.begin()+0);
+        permutationCase(ip,op1);
+        permutationCase(ip,op2);
+        return;
+    }else{
+        string op1 = op;
+        op1.push_back(ip[0]);
+        ip.erase(ip.begin()+0);
+        permutationCase(ip,op1);
+        return;
+    }
+       
+    
+}
+
+int main(){
+    string ip;
+    cin>>ip;
+    string op="";
+    permutationCase(ip,op);
+}
