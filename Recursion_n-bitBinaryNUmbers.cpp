9bd3c4bb8d1275dce28de569c22968a72a8a613e
saa27 <saakshi27050@gmail.com>
2020-12-09 23:30:58 +0530

    :white_check_mark: REcusrion ques

---
+++ Recursion_n-bitBinaryNUmbers.cpp
@@ -0,0 +1,35 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <stack>
+#include <iostream>
+#include <algorithm>
+#include <bits/stdc++.h>
+using namespace std;
+
+    void solve(int one, int zero, int n, string op){
+        if(n==0){
+            cout<<op<<"\n";
+            return;
+        }
+        string op1 = op;
+        op1.push_back('1');
+        solve(one+1,zero,n-1,op1);
+        if(one>zero){
+            string op2 = op;
+            op2.push_back('0');
+            solve(one,zero+1,n-1,op2);
+        }
+        return;
+    }  
+    
+    
+
+    int main(){
+        int n;
+        cin>>n;
+        string op="";
+        int one = 0;
+        int zero = 0;
+        solve(one,zero,n,op);
+    }
